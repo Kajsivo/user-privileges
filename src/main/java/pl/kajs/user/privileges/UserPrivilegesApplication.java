f950c598cf7360a2b2ce2871ba45bc5215a6c3a8
unknown <kajsiwo@gmail.com>
2016-04-09 12:41:25 +0200

    init

---
+++ src/main/java/pl/kajs/user/privileges/UserPrivilegesApplication.java
@@ -0,0 +1,12 @@
+package pl.kajs.user.privileges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPrivilegesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserPrivilegesApplication.class, args);
+	}
+}
